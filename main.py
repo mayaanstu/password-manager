ac8880f5672aa82331a92fa33875749910aa73a1
Muhammad Ayaan <m.ayaan.prf@gmail.com>
2026-07-11 13:39:12 +0500

    Completed terminal password manager

--- main.py
+++ main.py
@@ -6,30 +6,54 @@ print("1. Add Password")
 print("2. View Passwords")
 print("3. Search")
 print("4. Exit")
+
 choice = input("\nChoose an option (1-4): ")
 print("You chose:", choice)
+
 if choice == "1":
     print("You chose to add a password.")
     website = input("Website: ")
-    username = input("Username: ") 
-    password = input("\nEnter your password (3-20 characters): ")
+    username = input("Username: ")
+    password = input("Enter your password (3-20 characters): ")
     if len(password) < 3 or len(password) > 20:
         print("Password must be between 3 and 20 characters.")
-        repeat_password = input("Please re-enter your password: ")
-        if password != repeat_password:
-            print("Passwords do not match. Please try again.")
-            password = input("Enter your password (3-20 characters): ") 
     else:
         print("Adding password...")
-    with open("passwords.txt", "a") as file:
-        file.write(f"{website},{username},{password}\n")
+        with open("passwords.txt", "a") as file:
+            file.write(f"{website},{username},{password}\n")
+        print("Password saved successfully!")
 
-    print("Password saved successfully!")
 elif choice == "2":
-    print("You chose to view passwords.")
+    print("Saved Passwords:\n")
+    with open("passwords.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            website, username, password = line.split(",")
+            print(f"Website : {website}")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+            print("_________________________")
+
 elif choice == "3":
     print("You chose to search for a password.")
+    websiteinp = input("Enter the website you want to search for: ").lower()
+    found = False
+    with open("passwords.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            website, username, password = line.split(",")
+            if websiteinp == website.lower():
+                found = True
+                print("Password found!:")
+                print(f"Website : {website}")
+                print(f"Username: {username}")
+                print(f"Password: {password}")
+                print("_________________________")
+    if not found:
+        print("No password found for the specified website.")
+
 elif choice == "4":
     print("Exiting the program.")
+
 else:
     print("Invalid choice. Please select a valid option.")
