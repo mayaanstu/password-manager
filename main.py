7a1d615349f82b3d7510e1d03825ae44cb5c8421
Muhammad Ayaan <m.ayaan.prf@gmail.com>
2026-07-21 13:00:36 +0500

    Added master password and password strength checker

--- main.py
+++ main.py
@@ -1,6 +1,206 @@
 from getpass import getpass
-master_password = "" 
-while True and master_password == "":
+
+try:
+    with open("master.txt", "r") as file:
+        master_password = file.read().strip()
+except FileNotFoundError:
+    master_password = ""
+
+def add_password():
+    print("You chose to add a password.")
+    website = input("Website: ")
+    username = input("Username: ")
+    password = input("Enter your password (3-20 characters): ")
+    if len(password) < 3 or len(password) > 20:
+       print("Password must be between 3 and 20 characters.")
+    else:
+       print("Adding password...")
+       with open("passwords.txt", "a") as file:
+            file.write(f"{website},{username},{password}\n")
+    print("Password saved successfully!")
+
+
+def view_passwords():
+    print("You chose to view saved passwords.")
+    print("Saved Passwords:\n")
+    with open("passwords.txt", "r") as file:    
+        for line in file:
+            line = line.strip()
+            website, username, password = line.split(",")
+            print(f"Website : {website}")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+            print("_________________________")
+
+
+def search_password():
+    print("You chose to search for a password.")
+    website_to_search = input("Enter the website you want to search for: ").lower()
+    found = False
+    with open("passwords.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            website, username, password = line.split(",")
+            if website_to_search == website.lower():
+                found = True
+                print("Password found!")
+                print(f"Website : {website}")
+                print(f"Username: {username}")
+                print(f"Password: {password}")
+                print("_________________________")
+        if not found:
+            print("No password found for the specified website.")
+
+
+def delete_password():
+    print("You chose to delete a website entry.")
+    website_to_delete = input("Enter the website entry you want to delete: ").lower()
+    found = False
+    with open("passwords.txt", "r") as file:
+        lines = file.readlines()
+    with open("passwords.txt", "w") as file:
+        for line in lines:
+            line = line.strip()
+            website, username, password = line.split(",")
+            if website_to_delete != website.lower():
+                file.write(line + "\n")
+            else:
+                found = True
+                print(f"Website entry for {website} deleted successfully.")
+    if not found:
+        print("No website entry found for the specified website.")
+
+
+def change_password():
+    print("You chose to change a password.")
+    website_to_change = input("Enter the website for which you want to change the password: ").lower()
+    found = False
+    with open("passwords.txt", "r") as file:
+        lines = file.readlines()
+    with open("passwords.txt", "w") as file:
+        for line in lines:
+            line = line.strip()
+            website, username, password = line.split(",")
+            if website_to_change == website.lower():
+                found = True
+                new_password = getpass("Enter the new password (3-20 characters): ")
+                if len(new_password) < 3 or len(new_password) > 20:
+                    print("Password must be between 3 and 20 characters.")
+                    file.write(line + "\n")  
+                else:
+                    file.write(f"{website},{username},{new_password}\n")
+                    print(f"Password for {website} changed successfully.")
+            else:
+                 file.write(line + "\n")
+    if not found:
+        print("No website entry found for the specified website.")
+
+def generate_password():
+    print("You chose to generate a password.")
+    import random
+    import string
+
+    length = int(input("Enter the desired password length (3-20): "))
+    if length < 3 or length > 20:
+        print("Password length must be between 3 and 20 characters.")
+    else:
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(length))
+        print("Generated password:", password)
+    
+
+def change_master_password():
+        global master_password
+        print("You chose to change/add the master password.")
+        if master_password != "":
+            master_password_input = getpass("Enter the current master password: ")
+            while master_password_input != master_password:
+                print("Incorrect master password.")
+                master_password_input = getpass("Enter the current master password again: ")
+            changed_master_password = getpass("Enter the new master password: ")
+            while len(changed_master_password) > 20:
+                print("Master password must be less than 20 characters.")
+                changed_master_password = getpass("Enter the new master password: ")
+            else :
+                master_password = changed_master_password
+                with open("master.txt", "w") as file:
+                    file.write(master_password)
+                print("Master password changed successfully.")
+        else:
+            master_password = input("Create a master password: ")
+            if len(master_password) > 20:
+                print("Master password must be less than 20 characters.")
+            else :
+                with open("master.txt", "w") as file:
+                    file.write(master_password)
+                print("Master password changed successfully.")
+            
+
+def verify_master_password():
+    global master_password
+    if master_password != "":
+        master_password_input = getpass("Enter the master password: ")
+        while master_password_input != master_password:
+            print("Incorrect master password.")
+            master_password_input = getpass("Enter the master password again: ")
+        print("Master password verified successfully.")
+    else:
+        print("No master password set. Please set a master password first.")
+        change_master_password()
+
+
+def check_password_strength():
+    flag = False
+    print("You chose to check password strength.")
+    password_to_check = input("Enter the password you want to check: ")
+    if len(password_to_check) < 8:
+        print("Weak password: Password should be at least 8 characters long.")
+        flag = False
+    elif not any(char.isdigit() for char in password_to_check):
+        print("Weak password: Password should contain at least one digit.")
+        flag = False
+    elif not any(char.isupper() for char in password_to_check):
+        print("Weak password: Password should contain at least one uppercase letter.")
+        flag = False
+    elif not any(char.islower() for char in password_to_check):
+        print("Weak password: Password should contain at least one lowercase letter.")
+        flag = False
+    elif not any(char in "!@#$%^&*()-_=+[{]}|\;:'\,<.>/?`~" for char in password_to_check):
+        print("Weak password: Password should contain at least one special character.")
+        flag = False
+    else:
+        print("Strong password.")
+        flag = True
+    while flag == False:
+        password_to_check = input("Enter the password you want to check again: ")
+        if len(password_to_check) < 8:
+            print("Weak password: Password should be at least 8 characters long.")
+            flag = False
+        elif not any(char.isdigit() for char in password_to_check):
+            print("Weak password: Password should contain at least one digit.")
+            flag = False
+        elif not any(char.isupper() for char in password_to_check):
+            print("Weak password: Password should contain at least one uppercase letter.")
+            flag = False
+        elif not any(char.islower() for char in password_to_check):
+            print("Weak password: Password should contain at least one lowercase letter.")
+            flag = False
+        elif not any(char in "!@#$%^&*()-_=+[{]}|\;:'\,<.>/?`~" for char in password_to_check):
+            print("Weak password: Password should contain at least one special character.")
+            flag = False
+        else:
+            print("Strong password.")
+            flag = True
+
+if master_password == "":
+    print("No master password set. Please set a master password first.")
+    change_master_password()
+entered_master_password = input("Enter the master password: ")
+while entered_master_password != master_password:
+    print("Incorrect master password.")
+    entered_master_password = input("Enter the master password again: ")
+
+while True: 
     print("_________________________")
     print("    PASSWORD MANAGER")
     print("_________________________")
@@ -12,133 +212,11 @@ while True and master_password == "":
     print("5. Change Password")
     print("6. Generate Password")
     print("7. Change Master Password")
-    print("8. Exit The Program")
-
-    def add_password():
-        print("You chose to add a password.")
-        website = input("Website: ")
-        username = input("Username: ")
-        password = input("Enter your password (3-20 characters): ")
-        if len(password) < 3 or len(password) > 20:
-           print("Password must be between 3 and 20 characters.")
-        else:
-           print("Adding password...")
-           with open("passwords.txt", "a") as file:
-                file.write(f"{website},{username},{password}\n")
-        print("Password saved successfully!")
+    print("8. Check Password Strength")
+    print("9. Exit The Program")
 
 
-    def view_passwords():
-        print("You chose to view saved passwords.")
-        print("Saved Passwords:\n")
-        with open("passwords.txt", "r") as file:
-            for line in file:
-                line = line.strip()
-                website, username, password = line.split(",")
-                print(f"Website : {website}")
-                print(f"Username: {username}")
-                print(f"Password: {password}")
-                print("_________________________")
-
-
-    def search_password():
-        print("You chose to search for a password.")
-        website_to_search = input("Enter the website you want to search for: ").lower()
-        found = False
-        with open("passwords.txt", "r") as file:
-            for line in file:
-                line = line.strip()
-                website, username, password = line.split(",")
-                if website_to_search == website.lower():
-                    found = True
-                    print("Password found!")
-                    print(f"Website : {website}")
-                    print(f"Username: {username}")
-                    print(f"Password: {password}")
-                    print("_________________________")
-            if not found:
-                print("No password found for the specified website.")
-
-
-    def delete_password():
-        print("You chose to delete a website entry.")
-        website_to_delete = input("Enter the website entry you want to delete: ").lower()
-        found = False
-        with open("passwords.txt", "r") as file:
-            lines = file.readlines()
-        with open("passwords.txt", "w") as file:
-            for line in lines:
-                line = line.strip()
-                website, username, password = line.split(",")
-                if website_to_delete != website.lower():
-                    file.write(line + "\n")
-                else:
-                    found = True
-                    print(f"Website entry for {website} deleted successfully.")
-        if not found:
-            print("No website entry found for the specified website.")
-
-
-    def change_password():
-        print("You chose to change a password.")
-        website_to_change = input("Enter the website for which you want to change the password: ").lower()
-        found = False
-        with open("passwords.txt", "r") as file:
-            lines = file.readlines()
-        with open("passwords.txt", "w") as file:
-            for line in lines:
-                line = line.strip()
-                website, username, password = line.split(",")
-                if website_to_change == website.lower():
-                    found = True
-                    new_password = getpass("Enter the new password (3-20 characters): ")
-                    if len(new_password) < 3 or len(new_password) > 20:
-                        print("Password must be between 3 and 20 characters.")
-                        file.write(line + "\n")  
-                    else:
-                        file.write(f"{website},{username},{new_password}\n")
-                        print(f"Password for {website} changed successfully.")
-                else:
-                    file.write(line + "\n")
-        if not found:
-            print("No website entry found for the specified website.")
-
-    def generate_password():
-        print("You chose to generate a password.")
-        import random
-        import string
-
-        length = int(input("Enter the desired password length (3-20): "))
-        if length < 3 or length > 20:
-            print("Password length must be between 3 and 20 characters.")
-        else:
-            characters = string.ascii_letters + string.digits + string.punctuation
-            password = ''.join(random.choice(characters) for _ in range(length))
-            print("Generated password:", password)
-    
-
-    def change_master_password():
-                print("You chose to change/add the master password.")
-                if master_password == "":
-                    new_master_password = getpass("Enter the new master password (3-20 characters): ")
-                    if len(new_master_password) < 3 or len(new_master_password) > 20:
-                        print("Master password must be between 3 and 20 characters.")
-                    else:
-                        global master_password
-                        master_password = new_master_password
-                        print("Master password set successfully.")
-                else:
-                    current_master_password = getpass("Enter the current master password: ")
-                    new_master_password = getpass("Enter the new master password (3-20 characters): ")
-                    if len(new_master_password) < 3 or len(new_master_password) > 20:
-                        print("Master password must be between 3 and 20 characters.")
-                    else:
-                        global master_password
-                        master_password = new_master_password
-                        print("Master password changed successfully.")    
-
-
-    choice = input("\nChoose an option (1-7): ")
+    choice = input("\nChoose an option (1-9): ")
     print("You chose:", choice)
 
     if choice == "1":
@@ -163,6 +241,9 @@ while True and master_password == "":
         change_master_password()
 
     elif choice == "8":
+        check_password_strength()
+
+    elif choice == "9":
         print("Exiting the program...")
         break
 
